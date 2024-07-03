c8b71c5748c9a7627793f0605a5ccf0007bb29f7
Divyesh Mali <medivyeshmali@gmail.com>
2024-07-03 20:05:30 +0530

    Implement Drawer & Tab Navigation

--- App.js
+++ App.js
@@ -1,49 +1,21 @@
-// Navigation is implemented via two methods : 1. Navigation using the Hook method 2. Navigation using the Prop method
-// Use the Hook method when you want to navigate between screens in the app. Use the Prop method when you want to pass the navigation prop to the component.
-// Its recommended to use navigation props for all screen components and use navigation hooks only when necessary
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreenHook from './screens/HomeScreenHook';
-import AboutScreen from './screens/AboutScreen';
-import HomeScreenProp from './screens/HomeScreenProp';
-import { Text, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { View, Text } from 'react-native'
+import React from 'react'
+import CourseListScreen from './screens/CourseListScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import SettingsScreen from './screens/SettingsScreen'
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='HomeWithProp' 
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#6a51ae"
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-        headerRight: () => (
-          <Pressable onPress={() => alert("Menu button pressed!")}>
-            <Text style={{color: "#fff", fontSize: 16}}>Menu</Text>
-          </Pressable>
-        ),
-        contentStyle: {
-          backgroundColor: "#e8e4f3",
-        }
-      }}>
-        <Stack.Screen name="HomeWithProp" component={HomeScreenProp} options={{
-          title: "Welcome Home",
-          
-
-        }}/>
-        <Stack.Screen name="HomeWithHook" component={HomeScreenHook}/>
-        <Stack.Screen name="About" component={AboutScreen} initialParams={{name: "Guest",}}
-          // options={( { route} ) => ({      // This is the way to set the title of the screen dynamically via the route prop
-          //   title: route.params.name,      // The prop 'name: "Vishwas" is passed to the HomeScreenProps component. The name prop is accessed via the route.params object. The title of the screen is set to the value of the name prop.
-          // })}                                 
-        />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Couse List" component={CourseListScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen}/>
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
-
---
+++ screens/CourseListScreen.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const CourseListScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>CourseListScreen</Text>
+    </View>
+  );
+};
+
+export default CourseListScreen;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+        },
+
+        text: {
+            fontSize: 24,
+            fontWeight: "bold",
+            marginBottom: 16,
+        }
+    })
+
+
